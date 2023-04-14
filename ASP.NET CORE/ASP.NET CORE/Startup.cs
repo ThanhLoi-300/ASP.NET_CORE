09db28b9a66cf116f9447b526da0a071397b4983
Đoàn Thành Lợi <100418351+ThanhLoi-300@users.noreply.github.com>
2023-04-14 22:26:22 +0700

    Revert Commit

---
+++ ASP.NET CORE/ASP.NET CORE/Startup.cs
@@ -0,0 +1,47 @@
+﻿namespace ASP.NET_CORE
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            // Thêm session
+            services.AddSession();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseSession(); // Sử dụng session
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
